7f5f2f038fec99c28fae2fbdfeea5f7298091a6a
ParvizHacili <parviz.hajili@gmail.com>
2022-04-11 18:12:51 +0400

    Add project files.

---
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+        private readonly IOrderWriteRepository _orderWriteRepository;
+        private readonly IOrderReadRepository _orderReadRepository;
+        private readonly ICustomerWriteRepository _customerWriteRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository, IOrderWriteRepository orderWriteRepository,IOrderReadRepository orderReadRepository,ICustomerWriteRepository customerWriteRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _customerWriteRepository= customerWriteRepository;
+            _orderReadRepository = orderReadRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Salam");
+        }
+
+       
+    }
+}
